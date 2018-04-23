3c5b74a9eed92b8c9c1a8c528ebdccb59ef7a87a
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2018-04-23 16:43:14 +0530

    Add files via upload

---
+++ ecal.cc
@@ -0,0 +1,97 @@
+#include "ECalDetectorConstruction.hh"
+#include "ECalActionInitialization.hh"
+
+#ifdef G4MULTITHREADED
+#include "G4MTRunManager.hh"
+#else
+#include "G4RunManager.hh"
+#endif
+
+#include "G4UImanager.hh"
+#include "FTFP_BERT.hh"
+#include "G4StepLimiterPhysics.hh"
+
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+#ifdef G4UI_USE
+#include "G4UIExecutive.hh"
+#endif
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int main(int argc,char** argv)
+{
+#ifdef G4UI_USE
+  // Detect interactive mode (if no arguments) and define UI session
+  //
+  G4UIExecutive* ui = 0;
+  if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+  }
+#endif
+
+  // Construct the default run manager
+  //
+#ifdef G4MULTITHREADED
+  auto runManager = new G4MTRunManager;
+#else
+  auto runManager = new G4RunManager;
+#endif
+
+  // Mandatory user initialization classes
+  runManager->SetUserInitialization(new ECalDetectorConstruction);
+
+  auto physicsList = new FTFP_BERT;
+  physicsList->RegisterPhysics(new G4StepLimiterPhysics());
+  runManager->SetUserInitialization(physicsList);
+
+  // User action initialization
+  runManager->SetUserInitialization(new ECalActionInitialization());
+
+#ifdef G4VIS_USE
+  // Visualization manager construction
+  auto visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+#endif
+    
+  // Get the pointer to the User Interface manager
+  auto UImanager = G4UImanager::GetUIpointer();
+
+  if ( argc != 1 ) {
+    // execute an argument macro file if exist
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else {
+#ifdef G4UI_USE
+#ifdef G4VIS_USE
+    UImanager->ApplyCommand("/control/execute init_vis.mac"); 
+#else
+    UImanager->ApplyCommand("/control/execute init.mac"); 
+#endif
+    if (ui->IsGUI()) {
+         UImanager->ApplyCommand("/control/execute gui.mac");
+    }     
+    // start interactive session
+    ui->SessionStart();
+    delete ui;
+#endif
+  }
+
+  // Job termination
+  // Free the store: user actions, physics_list and detector_description are
+  // owned and deleted by the run manager, so they should not be deleted 
+  // in the main() program !
+
+#ifdef G4VIS_USE
+  delete visManager;
+#endif
+  delete runManager;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
