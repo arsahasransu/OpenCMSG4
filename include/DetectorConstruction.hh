ba3e0749bb1cf92c22a9192c5c6857881740ce9e
arsahasransu <a.r.sahasransu@gmail.com>
2019-05-14 22:15:56 +0200

    New class added for Electronic Calorimeter

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -21,55 +21,52 @@ class G4GenericMessenger;
 
 class DetectorConstruction : public G4VUserDetectorConstruction
 {
-  public:
-    DetectorConstruction();
-    virtual ~DetectorConstruction();
-    
-    virtual G4VPhysicalVolume* Construct();
-    virtual void ConstructSDandField();
-    
-    void ConstructMaterials();
-    
-  private:
-    void DefineCommands();
-
-    G4GenericMessenger* fMessenger;
-    
-    static G4ThreadLocal MagneticField* fMagneticField;
-    static G4ThreadLocal G4FieldManager* fFieldMgr;
-    
-    G4LogicalVolume* logicWorld;
-    G4LogicalVolume* ecalLogical;
-    std::vector< std::vector<G4LogicalVolume*> > cellEcalBarLogical;
-    G4LogicalVolume* ecalECLogical_r;
-    G4LogicalVolume* ecalECLogical_l;
-    std::vector<G4LogicalVolume*> cellEcalECLogical_r;
-    std::vector<G4LogicalVolume*> cellEcalECLogical_l;
-    G4LogicalVolume* fMagneticLogical;
-    G4LogicalVolume* trackerLogical;
-    G4LogicalVolume* ecalPSLogical_r;
+public:
+  DetectorConstruction();
+  virtual ~DetectorConstruction();
+  
+  virtual G4VPhysicalVolume* Construct();
+  virtual void ConstructSDandField();
+  
+  void ConstructMaterials();
+  
+private:
+  void DefineCommands();
+  
+  G4GenericMessenger* fMessenger;
+  
+  static G4ThreadLocal MagneticField* fMagneticField;
+  static G4ThreadLocal G4FieldManager* fFieldMgr;
+  
+  G4LogicalVolume* logicWorld;
+  G4LogicalVolume* ecalLogical;
+  G4LogicalVolume* ecalECLogical_r;
+  G4LogicalVolume* ecalECLogical_l;
+  G4LogicalVolume* fMagneticLogical;
+  G4LogicalVolume* trackerLogical;
+  G4LogicalVolume* ecalPSLogical_r;
     G4LogicalVolume* ecalPSLogical_l;
-    
-    std::vector<G4VisAttributes*> fVisAttributes;
-    
-		// Tracker Variables
-	std::vector<G4Material*> pixBarMat;
-	std::vector<G4Material*> pixECMat;
-	std::vector<G4Material*> TIBMat;
-	std::vector<G4Material*> TOBMat;
-	std::vector<G4Material*> TECMat;
-	std::vector<G4Material*> PShowerMat;
-	std::vector<G4LogicalVolume*> PIB_Logical;
-	std::vector<G4LogicalVolume*> PID_Logical;
-	std::vector<G4LogicalVolume*> TIB_Logical;
-	std::vector<G4LogicalVolume*> TID_Logical;
-	std::vector<G4LogicalVolume*> TOB_Logical;
-	std::vector<G4LogicalVolume*> TEC_Logical;
-	std::vector<G4LogicalVolume*> cellEcalPS_l;
-	std::vector<G4LogicalVolume*> cellEcalPS_r;
+  
+  std::vector<G4VisAttributes*> fVisAttributes;
+  
+  // Tracker Variables
+  std::vector<G4Material*> pixBarMat;
+  std::vector<G4Material*> pixECMat;
+  std::vector<G4Material*> TIBMat;
+  std::vector<G4Material*> TOBMat;
+  std::vector<G4Material*> TECMat;
+  std::vector<G4Material*> PShowerMat;
+  std::vector<G4LogicalVolume*> PIB_Logical;
+  std::vector<G4LogicalVolume*> PID_Logical;
+  std::vector<G4LogicalVolume*> TIB_Logical;
+  std::vector<G4LogicalVolume*> TID_Logical;
+  std::vector<G4LogicalVolume*> TOB_Logical;
+  std::vector<G4LogicalVolume*> TEC_Logical;
+  std::vector<G4LogicalVolume*> cellEcalPS_l;
+  std::vector<G4LogicalVolume*> cellEcalPS_r;
 
-	G4int ecalMode;
-	G4int tracMode;
+  G4int ecalMode;
+  G4int tracMode;
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -2,6 +2,7 @@
 #include "MagneticField.hh"
 #include "Constants.hh"
 #include "VolumeControl.hh"
+#include "ECalConstruction.hh"
 
 #include "G4FieldManager.hh"
 #include "G4TransportationManager.hh"
@@ -44,21 +45,13 @@ G4ThreadLocal G4FieldManager* DetectorConstruction::fFieldMgr = 0;
 
 DetectorConstruction::DetectorConstruction()
 : G4VUserDetectorConstruction(), 
-  fMessenger(nullptr), cellEcalBarLogical(nofEmBarEta),
-  cellEcalECLogical_r(kNofEmECCells), cellEcalECLogical_l(kNofEmECCells),
+  fMessenger(nullptr),
   fMagneticLogical(nullptr),
   fVisAttributes(), 
   PIB_Logical(PIB_num), PID_Logical(PID_num), TIB_Logical(TIB_num), TID_Logical(TID_num), TOB_Logical(TOB_num),
   TEC_Logical(TEC_num),
   ecalMode(111), tracMode(111111)
 {
-  
-	for(G4int num=0; num<nofEmBarEta; num++)
-	{
-		std::vector<G4LogicalVolume*> temp(nofEmBarPhi);
-		cellEcalBarLogical[num] = temp;
-	}
-
   // define commands for this class
   DefineCommands();
 }
@@ -597,36 +590,10 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	ecalLogical = new G4LogicalVolume(ecalSolid,world_mat,"ECalLogical");
 	new G4PVPlacement(0, G4ThreeVector(), ecalLogical, "ECalEnvelope", logicWorld, false, 0);
 
-	for(G4int copyNo=0; copyNo<nofEmBarCells; copyNo++)
-	{
-
-		G4double eta_max = 1.479;
-		G4double eta_step = eta_max/85;
-		G4double y = 1290.0*mm;
-		G4double dy = 220*mm;
-		
-		G4int etaNum = (copyNo+(nofEmBarPhi-360)*30)/nofEmBarPhi;
-		G4int phiNum = (copyNo+(nofEmBarPhi-360)*30)%nofEmBarPhi;
-
-		G4double eta_b = -eta_max+etaNum*eta_step;
-		G4double eta_e = -eta_max+(etaNum+1)*eta_step;
-		G4double theta_b = 2*atan(exp(-eta_b))*180/M_PI;
-		G4double theta_e = 2*atan(exp(-eta_e))*180/M_PI;
-		G4double theta_avg = 0.5*(theta_b+theta_e);
-		G4double rMin = y/sin(theta_avg*M_PI/180);
-		G4double rMax = rMin+dy;
-  		auto cellSolid 
-			= new G4Sphere("cellSolid", rMin,rMax,phiNum*deg, 1.*deg, theta_e*deg, (theta_b-theta_e)*deg);
-
-		cellEcalBarLogical[etaNum][phiNum]
-			= new G4LogicalVolume(cellSolid,ecal_mat,"cellLogical");
-		if(ECalBar)
-		new G4PVPlacement(0, G4ThreeVector(), cellEcalBarLogical[etaNum][phiNum], "ECalCell", ecalLogical, false, copyNo, false);
+	ECalConstruction* ecal = new ECalConstruction();
+	if(ECalBar) {
+	  ecal->makeBarrel(ecal_mat, ecalLogical, fVisAttributes);
 	}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////// ECAL ENDCAP PLACEMENT ///////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// ECal Endcap Envelope
 	G4double ecalECenv_z = 250.0;
@@ -637,40 +604,11 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	ecalECLogical_l = new G4LogicalVolume(ecalECSolid, world_mat, "ECalECLogical");
 	new G4PVPlacement(0, G4ThreeVector(0, 0, -(3205+110)*mm), ecalECLogical_l, "ECalECEnvelopeLeft", logicWorld, false, 0, true);
 
-	// ECal End Cap Parameterised Placement of Crytals
-
-	G4double rmin_EC = 3205*tan(2*atan(exp(-3))), xpos_EC, ypos_EC;
-	G4double rmax_EC = 3205*tan(2*atan(exp(-1.479+0.04)));
-	G4double y1=25.7, y2=26.9, len=220.0;
-	int k = 0;
-	for(int i=-70; i<70; i++)
-	for(int j=-70; j<70; j++)
-	if(k<kNofEmECCells)
-	{
-		G4double d1 = sqrt(pow(y2*(i+0.5),2)+pow(y2*(j+0.5),2));
-		if(d1>rmin_EC && d1<rmax_EC)
-		{
-			xpos_EC = y2*(i+0.5);
-			ypos_EC = y2*(j+0.5);
-			G4ThreeVector* d_r = new G4ThreeVector(xpos_EC, ypos_EC, (3205+110+1300));
-			auto cellEcalECSolid_r = new G4Trap("cellEcalECSolid_r", 0.5*len*mm, d_r->getTheta()*rad,
-		            d_r->getPhi()*rad, 0.5*y1*mm, 0.5*y1*mm, 0.5*y1*mm, 0*deg, 0.5*y2*mm,
-	             	0.5*y2*mm, 0.5*y2*mm, 0*deg);
-			cellEcalECLogical_r[k] = new G4LogicalVolume(cellEcalECSolid_r, ecal_mat, "CellEcalECLogical_r");
-			G4ThreeVector* d_l = new G4ThreeVector(xpos_EC, ypos_EC, -(3205+110+1300));
-			auto cellEcalECSolid_l = new G4Trap("cellEcalECSolid_l", 0.5*len*mm, d_l->getTheta()*rad,
-		            d_l->getPhi()*rad, 0.5*y2*mm, 0.5*y2*mm, 0.5*y2*mm, 0*deg, 0.5*y1*mm,
-	             	0.5*y1*mm, 0.5*y1*mm, 0*deg);
-			cellEcalECLogical_l[k] = new G4LogicalVolume(cellEcalECSolid_l, ecal_mat, "CellEcalECLogical_l");
-				if(ECalR)
-				new G4PVPlacement(0, G4ThreeVector(xpos_EC*mm, ypos_EC*mm, 0.), cellEcalECLogical_r[k], "CellEcalEC_r", ecalECLogical_r, false, k, false);
-				if(ECalL)
-				new G4PVPlacement(0, G4ThreeVector(xpos_EC*mm, ypos_EC*mm, 0.), cellEcalECLogical_l[k], "CellEcalEC_l", ecalECLogical_l, false, k, false);
-			k++;
-		}
+	if(ECalR || ECalL) {
+	  ecal->makeEndCap(ecal_mat, ecalECLogical_r, ecalECLogical_l, ECalR, ECalL, fVisAttributes);
 	}
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////// PRE-SHOWER PLACEMENT ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -716,32 +654,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	//visAttributes->SetForceLineSegmentsPerCircle(10);
   ecalLogical->SetVisAttributes(visAttributes);
   fVisAttributes.push_back(visAttributes);
-
-	for(G4int copyNo=0; copyNo<nofEmBarCells; copyNo++)
-	{
-  visAttributes = new G4VisAttributes(G4Colour(0.8888,0,0));
-//  visAttributes->SetVisibility(false);
-	//visAttributes->SetForceLineSegmentsPerCircle(10);
-	if(copyNo<360*30)   cellEcalBarLogical[copyNo/360][copyNo%360]->SetVisAttributes(visAttributes);
-	else  cellEcalBarLogical[(copyNo+(nofEmBarPhi-360)*30)/nofEmBarPhi][(copyNo+(nofEmBarPhi-360)*30)%nofEmBarPhi]->SetVisAttributes(visAttributes);
-  fVisAttributes.push_back(visAttributes);
-	}
-
-	for(G4int copyNo=0; copyNo<kNofEmECCells; copyNo++)
-	{
-  visAttributes = new G4VisAttributes(G4Colour(0.9115,0.4112,0.0042));
-//  visAttributes->SetVisibility(false);
-	//visAttributes->SetForceLineSegmentsPerCircle(10);
-  cellEcalECLogical_r[copyNo]->SetVisAttributes(visAttributes);
-  fVisAttributes.push_back(visAttributes);
-
-  visAttributes = new G4VisAttributes(G4Colour(0.9115,0.4112,0.0042));
-//  visAttributes->SetVisibility(false);
-	//visAttributes->SetForceLineSegmentsPerCircle(10);
-  cellEcalECLogical_l[copyNo]->SetVisAttributes(visAttributes);
-  fVisAttributes.push_back(visAttributes);
-	}
-
+  
   visAttributes = new G4VisAttributes(G4Colour(0,0,0.8888));
   visAttributes->SetVisibility(false);
 	//visAttributes->SetForceLineSegmentsPerCircle(10);
