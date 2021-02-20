633e6d29a277e677e20b889e9dad5569965cd6af
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2021-02-20 17:31:20 +0100

    Commit to merge with trackerPixelation

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -17,19 +17,14 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 RunAction::RunAction(EventAction* eventAction):
-	G4UserRunAction(),
-	runMessenger(nullptr),
-	fEventAction(eventAction),
-	outRootName("OpenCMSG4_root.root")
-{
-
+  G4UserRunAction(),
+  runMessenger(nullptr),
+  fEventAction(eventAction),
+  outRootName("OpenCMSG4_root.root"){
+  
   // Define Commands for this class
   DefineCommands();
 
-  // Book histograms, ntuple
-  //
-  
-  
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -40,9 +35,9 @@ RunAction::~RunAction(){
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void RunAction::BeginOfRunAction(const G4Run*){ 
-
+  
   outRootFile = TFile::Open(outRootName,"RECREATE");
-
+  
   // Book histogram for ROOT Tree
   auto tree = new TTree("Events", "Events");
   fEventAction->EventTree(tree);
@@ -56,7 +51,7 @@ void RunAction::BeginOfRunAction(const G4Run*){
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void RunAction::EndOfRunAction(const G4Run*){
-
+  
   // Append the run time to a file
   end = time(NULL);
   std::cout<<"The time of the run was "<<difftime(end,start)
@@ -74,11 +69,11 @@ void RunAction::EndOfRunAction(const G4Run*){
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void RunAction::DefineCommands() {
-
+  
   runMessenger = new G4GenericMessenger(this,
-				      "/root/",
-				      "Analysis Settings");
-
+					"/root/",
+					"Analysis Settings");
+  
   // Control for the output file name
   auto& outRootNameCmd = runMessenger->DeclareProperty("setFileName", outRootName, "Name of the output ROOT file");
   outRootNameCmd.SetParameterName("setFileName", true);
@@ -87,4 +82,3 @@ void RunAction::DefineCommands() {
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
