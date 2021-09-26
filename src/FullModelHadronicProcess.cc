1a734bb7943e4767dd38f7587700a16934d22915
Soumya Dansana <soumyadansana@localhost.localdomain>
2021-09-26 14:55:30 +0200

    Fixed warning in FullModelHadronicProcess file

--- src/FullModelHadronicProcess.cc
+++ src/FullModelHadronicProcess.cc
@@ -555,8 +555,8 @@ void FullModelHadronicProcess::CalculateMomenta(
   const G4double twsup[] = { 1.0, 0.7, 0.5, 0.3, 0.2, 0.1 };
   G4double rand1 = G4UniformRand();
   G4double rand2 = G4UniformRand();
-  if( annihilation || (vecLen >= 6) ||
-      (modifiedOriginal.GetKineticEnergy()/GeV >= 1.0) &&
+  if( (annihilation || (vecLen >= 6) ||
+      (modifiedOriginal.GetKineticEnergy()/GeV >= 1.0)) &&
       (((originalIncident->GetDefinition() == G4KaonPlus::KaonPlus() ||
 	 originalIncident->GetDefinition() == G4KaonMinus::KaonMinus() ||
 	 originalIncident->GetDefinition() == G4KaonZeroLong::KaonZeroLong() ||
